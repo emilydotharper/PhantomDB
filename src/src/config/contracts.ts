706037836a0eb610741829211098d629f8cf59fc
emilydotharper <xgouygx8434@outlook.com>
2025-12-23 11:40:35 +0800

    Add contract metadata and RPC clients

---
+++ src/src/config/contracts.ts
@@ -0,0 +1,5 @@
+import { PHANTOM_DB_ABI } from '../abi/PhantomDBAbi';
+
+export const DEFAULT_CONTRACT_ADDRESS = '0x86FA30f522bA879A2d8EF43C150feba9B806D13C' as const;
+export const CONTRACT_ABI = PHANTOM_DB_ABI;
+
